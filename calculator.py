8804bee82b6bec69aa491faca08b8c26654b6085
RuchiBhandari-2208 <ruchibhandari228@gmail.com>
2026-06-09 22:59:30 +0530

    create calculator and pra2

---
+++ calculator.py
@@ -0,0 +1,36 @@
+while True:
+    print("\nCalculator")
+    print("1. Addition (+)")
+    print("2. Subtraction (-)")
+    print("3. Multiplication (*)")
+    print("4. Division (/)")
+ 
+    choice = input("Choose an operation (1-4): ")
+ 
+    a = float(input("Enter first number: "))
+    b = float(input("Enter second number: "))
+ 
+    if choice == "1":
+        print("Result =", a + b)
+ 
+    elif choice == "2":
+        print("Result =", a - b)
+ 
+    elif choice == "3":
+        print("Result =", a * b)
+ 
+    elif choice == "4":
+        if b != 0:
+            print("Result =", a / b)
+        else:
+            print("Error: Division by zero is not allowed.")
+ 
+    else:
+        print("Invalid choice!")
+ 
+    again = input("Do you want to continue? (yes/no): ")
+ 
+    if again.lower() != "yes":
+        print("Calculator Closed.")
+        break
+ 
